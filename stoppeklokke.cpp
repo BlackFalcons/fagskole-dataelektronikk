b8c49dbbd7e70036ef80ca49c94226e95eae300a
Falcon <37326776+BlackFalcons@users.noreply.github.com>
2022-12-15 01:21:06 +0100

    Improve a few declared variables

--- stoppeklokke.cpp
+++ stoppeklokke.cpp
@@ -8,7 +8,8 @@
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7); // Creates instance of LiquidCrystal object with the used pin numbers on the Arduino.
 
 // Declares three variables with the integer data type.
-int startStopButton{9}, resetButton{10}, recordedMinutes; // First and second variable is referencing the pin number of the button components used to start / stop and reset the display.
+const int startStopButtonPin{9}, resetButton{10}; // Create two constant variables with the type of integer, the const keyword prevents anyone form overwriting them.
+int recordedMinutes; // First and second variable is referencing the pin number of the button components used to start / stop and reset the display.
 float recordedTimeInSeconds = 0; // Declare variable with the floating point value data type.
 
 bool startStopButtonHasBeenPressed = false; // Declare variable with the boolean data type.
@@ -25,14 +26,14 @@ __attribute__((unused)) void loop() {
     lcd.setCursor(5, 0);
     if (digitalRead(resetButton)) // Reads the status of the resetButton pin and then resets the timer if it detects that the reset button is pressed.
     {
-		recordedTimeInSeconds = 0, recordedMinutes = 0; // Resetting the values storing the currently counted secondes and minutes.
+        recordedTimeInSeconds = 0, recordedMinutes = 0; // Resetting the values storing the currently counted secondes and minutes.
         lcd.clear(); // Uses the clear method from the LiquidCrystal object removing any value currently written on the screen.
         lcd.setCursor(5, 0);
         lcd.print((String) recordedMinutes + ":" + (String) recordedTimeInSeconds); // Updates LCD with: 0:0.0
         delay(500); // The delay function blocks the thread for x amount of milliseconds making the entire program pause for x amount of time.
     }
 
-    if (digitalRead(startStopButton)) // Detect if the 'start / stop' button has been pressed.
+    if (digitalRead(startStopButtonPin)) // Detect if the 'start / stop' button has been pressed.
     {
         startStopButtonHasBeenPressed = true;
         delay(500);
@@ -42,13 +43,13 @@ __attribute__((unused)) void loop() {
         {
             if (recordedTimeInSeconds >= 59.8) // Convert seconds to minutes, it is inaccurate by 20 ms beacuse the display is not responding fast enough.
             {
-              	tone(1, 2000); // Make a tone to warn the user that a minute has passed by.
+                tone(1, 2000); // Make a tone to warn the user that a minute has passed by.
                 recordedTimeInSeconds = 0; // Sets the second counter back to 0.
                 recordedMinutes++; // Adds a minute to the minute counter.
-              	delay(100); // Make the tone ring for 200 ms adding the delay after the if statement.
+                delay(100); // Make the tone ring for 200 ms adding the delay after the if statement.
             }
-          	delay(100);
-          	if (recordedTimeInSeconds == 0) noTone(1);
+            delay(100);
+            if (recordedTimeInSeconds == 0) noTone(1);
 
             recordedTimeInSeconds += 0.1; // Adding 100 ms to recorded time.
             lcd.clear();
@@ -56,7 +57,7 @@ __attribute__((unused)) void loop() {
 
             lcd.print((String) recordedMinutes + ":" + (String) recordedTimeInSeconds); // Type casting recordedMinutes and recordedTimeInSeconds to the type of string to print the values to the lcd screen.
 
-            if (digitalRead(startStopButton)) { // Detect if user is pressing button to stop.
+            if (digitalRead(startStopButtonPin)) { // Detect if user is pressing button to stop.
                 startStopButtonHasBeenPressed = false;
                 delay(500);
             }
